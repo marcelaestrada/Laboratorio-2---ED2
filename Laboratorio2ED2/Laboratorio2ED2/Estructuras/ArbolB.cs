20b41de49b2f7da51b05a842a420a9b44b82f1d3
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-09-20 16:31:09 -0600

    Metodo de insercion

--- Laboratorio2ED2/Laboratorio2ED2/Estructuras/ArbolB.cs
+++ Laboratorio2ED2/Laboratorio2ED2/Estructuras/ArbolB.cs
@@ -13,13 +13,45 @@ namespace Laboratorio2ED2
             int maxValues = maxValores(grado);
             int minValues = minValores(grado);
             int mitadValues = mitadValores(grado);
-        }
-        
-        void insertarValue(Nodo<T> root, int max, int min, int mitad)
-        {
-            
-        }
 
+            insertarValue(root, maxValues, minValues, mitadValues, value);
+        }
+        void insertarValue(Nodo<T> nodo, int max, int min, int mitad, T valor)
+        {
+            for(int i = 0; i < max; i++)
+            {
+                if (nodo.values[i].CompareTo(valor) == 1)
+                {
+                    if (nodo.hijoI != null)
+                    {
+                        insertarValue(nodo.hijoI, max, min, mitad, valor);
+                    }
+                    else
+                    {
+                        insertarOrdenar(nodo, valor);
+                        if (nodo.contador > max)
+                        {
+                            division(nodo, mitad, max);
+                        }
+                    }
+                }
+                else
+                {
+                    if (nodo.hijoD != null)
+                    {
+                        insertarValue(nodo.hijoD, max, min, mitad, valor);
+                    }
+                    else
+                    {
+                        insertarOrdenar(nodo, valor);
+                        if (nodo.contador > max)
+                        {
+                            division(nodo, mitad, max);
+                        }
+                    }
+                }
+            }
+        }
         int maxValores(int grado)
         {
             int maxValues = grado - 1;
@@ -52,15 +84,11 @@ namespace Laboratorio2ED2
             }
             return mitadValues;
         }
-        public Nodo<T> division(Nodo<T> valores, int mitad, int max)
+        public void division(Nodo<T> valores, int mitad, int max)
         {
-            Nodo<T> nuevoNodoI = new Nodo<T>();
             Nodo<T> nuevoNodoD = new Nodo<T>();
+            T valor = valores.values[mitad];
 
-            for (int i = 0; i < mitad; i++)
-            {
-                nuevoNodoI.values[i] = valores.values[i];
-            }
             for (int i = mitad + 1; i < max; i++)
             {
                 int j = 0;
@@ -68,10 +96,16 @@ namespace Laboratorio2ED2
                 j++;
             }
 
+            for (int i = mitad; i < max; i++)
+            {
+                valores.values[i] = default(T);
+            }
+
+
             if (valores.padre == null)
             {
-                valores.padre.values[0] = valores.values[mitad];
-                valores.padre.hijoI = nuevoNodoI;
+                valores.padre.values[0] = valor;
+                valores.padre.hijoI = valores;
                 valores.padre.hijoD = nuevoNodoD;
             }
             else
@@ -82,7 +116,7 @@ namespace Laboratorio2ED2
                     {
                         if (valores.padre.values[i]==null)
                         {
-                            valores.padre.values[i] = valores.values[mitad];
+                            valores.padre.values[i] = valor;
                         }
                     }
                     for(int i =0; i < valores.padre.contador; i++)
@@ -94,7 +128,7 @@ namespace Laboratorio2ED2
                             valores.padre.values[i + 1] = aux;
                         }
                     }
-                    valores.padre.hijoI = nuevoNodoI;
+                    valores.padre.hijoI = valores;
                     valores.padre.hijoD = nuevoNodoD;
                 }
                 else
@@ -102,7 +136,26 @@ namespace Laboratorio2ED2
                     division(valores.padre, mitad, max);
                 }
             }
-            return valores;
+        }
+        public void insertarOrdenar(Nodo<T> node, T valor)
+        {
+            for (int i = 0; i < root.contador; i++)
+            {
+                if (root.values[i] == null)
+                {
+                    root.values[i] = valor;
+                }
+            }
+            for (int i = 0; i < root.contador; i++)
+            {
+                if (root.values[i].CompareTo(root.values[i + 1]) == 1)
+                {
+                    T aux = root.values[i];
+                    root.values[i] = root.values[i + 1];
+                    root.values[i + 1] = aux;
+                }
+            }
+            node.contador++;
         }
         public void Eliminar(T value)
         {
