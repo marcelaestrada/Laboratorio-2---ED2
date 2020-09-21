eaa85e63cdc6e9e6a4ccb47be7e9c1efd4e15074
JoshuaValey <j97valey@gmail.com>
2020-09-21 14:08:35 -0600

    Estructura inicial Nodo

--- Laboratorio2ED2/Laboratorio2ED2/Estructuras/Nodo.cs
+++ Laboratorio2ED2/Laboratorio2ED2/Estructuras/Nodo.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Laboratorio2ED2
 {
-    class Nodo
+    internal class Nodo<T> where T: IComparable
     {
+        internal T[] Values { get; set; }
+
+        internal int Id { get; set; }
+
+        internal int Padre { get; set; }
+
+        internal int[] Hijos { get; set; }
+
+
     }
 }
