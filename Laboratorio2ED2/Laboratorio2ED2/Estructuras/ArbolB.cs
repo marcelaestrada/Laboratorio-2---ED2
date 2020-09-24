7774ff03d99a70ffbf0b31896494d233c8a3ced7
JoshuaValey <j97valey@gmail.com>
2020-09-24 03:12:40 -0600

    Método LeerLineaArchivo

--- Laboratorio2ED2/Laboratorio2ED2/Estructuras/ArbolB.cs
+++ Laboratorio2ED2/Laboratorio2ED2/Estructuras/ArbolB.cs
@@ -12,10 +12,18 @@ namespace Laboratorio2ED2
         int min;
         int max;
         int mitad;
-        FileStream path = File.Create(@"c:\ArchivoPeliculas.txt");
+        int encabezadoLength;
+        string ruta;
+        FileStream path;
 
-        internal ArbolB(int grado)
+        internal ArbolB(int grado, string ruta)
         {
+            this.ruta = ruta;
+
+            //Por motivos de depuración
+            this.ruta = @"c:\ArchivoPeliculas.txt";
+
+            path = File.Create(this.ruta);
             gradoArbol = grado;
             //valores minimos
             min = (grado - 1) / 2;
@@ -46,7 +54,7 @@ namespace Laboratorio2ED2
             if (id != 1)
             {
                 //si hay nodo, hay que hacer comprobaciones 
-                
+
             }
             else
             {
@@ -55,7 +63,7 @@ namespace Laboratorio2ED2
                 nuevoNodo.Id = id;
                 nuevoNodo.Padre = -1;
                 nuevoNodo.Values[0] = value;
-                nuevoNodo.Order = nuevoNodo.GradoArbol = gradoArbol; //creo que seria lo mismo
+                nuevoNodo.Order = nuevoNodo.Order = gradoArbol; //creo que seria lo mismo
                 nuevoNodo.CountOfValues++;
                 nuevoNodo.WriteToFile(path,1);
             }
@@ -68,12 +76,57 @@ namespace Laboratorio2ED2
 
         public void Eliminar(T value)
         {
+            //Ir a encabezado del archivo y recuperar id de raiz. 
+            int idRaiz = LeerRaizEncabezado();
+            EliminarValor(idRaiz, value);
             throw new NotImplementedException();
+        }
+
+        private void EliminarValor(int id, T value)
+        {
+            //file.Write(Encoding.UTF8.GetBytes(ToFixedLengthString()), 0, FixedSizedText);
+
+            Nodo<T> aux1 = new Nodo<T>(this.max, this.gradoArbol);
+            Nodo<T> aux2 = new Nodo<T>(this.max, this.gradoArbol);
+
+            int position = id;
+
+            string data = LeerLineaArchivo(position, aux1.FixedSizedText, ruta);
+
+
+            
+            //Ir a posicion de raiz o nodo en la recursividad. 
+            //Hacer la lectura de archivo y asignar valores al nodo aux1. 
+            //Evaluear si el valor buscado es el mismo en el campo hijos. 
+            //Si no, evaluear menor o mayor el x numero de veces
+            //Usar el número de validaciones para ir a la posicion del arreglo hijos. 
+            //Recuperar id del nodo hijo al que se movio. 
+
+            //REPETIR RECURCIVIDAD. 
+
+            //Al encontrar el valor evaluar casos de eliminacion. 
+
+        }
+
+        private string LeerLineaArchivo(int position, int fixedSizedText ,string ruta)
+        {
+            FileStream file = new FileStream(ruta, FileMode.Open, FileAccess.Read);
+            file.Seek( fixedSizedText * position + encabezadoLength, System.IO.SeekOrigin.Begin);
+            StreamReader reader = new StreamReader(file);
+            string respuesta = reader.ReadLine();
+            reader.Close();
+            file.Close();
+            return respuesta;
         }
 
         public T Buscar(T value)
         {
             throw new NotImplementedException();
+        }
+
+        private int LeerRaizEncabezado()
+        {
+           throw new NotImplementedException();
         }
     }
 }
