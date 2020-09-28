63a3d0f8345395ae95f8f3b52210af2622b5d84e
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-09-27 20:07:43 -0600

    Inicio de la api

--- Laboratorio2ED2/API/Controllers/MoviesController.cs
+++ Laboratorio2ED2/API/Controllers/MoviesController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models;
+using Laboratorio2ED2;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace API.Controllers
 {
@@ -11,6 +14,31 @@ namespace API.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
-       
+        int id = 1;
+        ArbolB<Pelicula> arbolPeliculas = new ArbolB<Pelicula>()
+        //api/movies/populate
+        [HttpPost("populate")]
+        public async Task<ActionResult> Post([FromBody] object peliculas)
+        {
+            List<Pelicula> dataSet = JsonConvert.DeserializeObject<List<Pelicula>>(content);
+
+            try
+            {
+                foreach (var item in dataSet)
+                {
+
+                    arbol.Insert(item);
+
+                }
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500);
+            }
+
+
+        }
     }
 }
---
+++ Laboratorio2ED2/API/Models/Orden.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Orden
+    {
+        public int orden { get; set; }
+    }
+}
