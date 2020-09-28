d7f91da79a857da0acd46232ce22f7e99248591c
JoshuaValey <j97valey@gmail.com>
2020-09-27 19:56:06 -0600

    Arreglar modelo y tipo parametro para eliminar.

--- Laboratorio2ED2/API/Controllers/MoviesController.cs
+++ Laboratorio2ED2/API/Controllers/MoviesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helpers;
+using API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +13,66 @@ namespace API.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+
        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         DELETE
+            ● Recibe un id en la ruta (/{id})
+            ● Elimina dicho elemento
+            ● Devuelve OK si no hubo error
+            ● Devuelve NotFound si el Id no existe
+            ● Devuelve InternalServerError si hubo error
+         */
+        //api/movies/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteElement(string id) 
+        {
+           
+            try
+            {
+                Storage.Instance.arbol.Eliminar(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500);
+            }
+            
+        }
+        
+
+
+
+
+
+
     }
 }
